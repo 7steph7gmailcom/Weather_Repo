08f80efe004ab1c96b8d297d48525d5e1394126f
stephaniecjohansson <7steph7@gmail.com>
2021-09-19 14:42:07 -0600

    new bootstrap

--- index.js
+++ index.js
@@ -1,47 +1,143 @@
-const apiKey = "86ea8aa56587bcd972b7b5efee08d7db";
-const base = "https//api.openweathermap.org/data/2.5/forecast";
+const apiKey = "&appid=86ea8aa56587bcd972b7b5efee08d7db";
+let city = $("#searchTerm").val();
+let date = new Date();
 
 
-const searchbox = document.querySelector(".search-box");
-searchbox.addEventListener("keypress", startQuery);
+$("#searchTerm").keypress(function(event) { 
+	
+	if (event.keyCode === 13) { 
+		event.preventDefault();
+		$("#searchBtn").click(); 
+	} 
+});
 
-function startQuery(event) {  
-    if (event.keyCode == 13) {
-        getResults(searchbox.value);
-        console.log(searchbox.value)
+$("#searchBtn").on("click", function() {
+
+  $('#forecastH5').addClass('show');
+
+  // get the value of the input from user
+  city = $("#searchTerm").val();
+  
+  // clear input box
+  $("#searchTerm").val("");  
+
+  // full url to call api
+  const queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
+
+  $.ajax({
+    url: queryUrl,
+    method: "GET"
+  })
+  .then(function (response){
+    console.log(response)
+    console.log(response.name)
+    console.log(response.weather[0].icon)
+    console.log(response.main.humidity)
+    console.log(response.wind.speed)
+
+    getCurrentConditions(response);
+    getCurrentForecast(response);
+    makeList();
+
+    })
+  });
+
+  function makeList() {
+    let listItem = $("<li>").addClass("list-group-item").text(city);
+    $(".list").append(listItem);
+  }
+
+  function getCurrentConditions (response) {
+
+    // get the temperature and convert to fahrenheit 
+    let tempF = (response.main.temp - 273.15) * 1.80 + 32;
+    tempF = Math.floor(tempF);
+
+    $('#currentCity').empty();
+
+    // get and set the content 
+    const card = $("<div>").addClass("card");
+    const cardBody = $("<div>").addClass("card-body");
+    const city = $("<h4>").addClass("card-title").text(response.name);
+    const cityDate = $("<h4>").addClass("card-title").text(date.toLocaleDateString('en-US'));
+    const temperature = $("<p>").addClass("card-text current-temp").text("Temperature: " + tempF + " °F");
+    const humidity = $("<p>").addClass("card-text current-humidity").text("Humidity: " + response.main.humidity + "%");
+    const wind = $("<p>").addClass("card-text current-wind").text("Wind Speed: " + response.wind.speed + " MPH");
+    const image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
+
+    // add to page
+    city.append(cityDate, image)
+    cardBody.append(city, temperature, humidity, wind);
+    card.append(cardBody);
+    $("#currentCity").append(card)
+   
+  }
+
+function getCurrentForecast () {
+  
+  $.ajax({
+    url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + apiKey,
+    method: "GET"
+  }).then(function (response){
+
+    console.log(response)
+    console.log(response.dt)
+    $('#forecast').empty();
+
+    // variable to hold response.list
+    let results = response.list;
+    console.log(results)
+    
+    //declare start date to check against
+    // startDate = 20
+    //have end date, endDate = startDate + 5
+
+    for (let i = 0; i < results.length; i++) {
+
+      let day = Number(results[i].dt_txt.split('-')[2].split(' ')[0]);
+      let hour = results[i].dt_txt.split('-')[2].split(' ')[1];
+      console.log(day);
+      console.log(hour);
+
+      if(results[i].dt_txt.indexOf("12:00:00") !== -1){
+        
+        // get the temperature and convert to fahrenheit 
+        let temp = (results[i].main.temp - 273.15) * 1.80 + 32;
+        let tempF = Math.floor(temp);
+
+        const card = $("<div>").addClass("card col-md-2 ml-4 bg-primary text-white");
+        const cardBody = $("<div>").addClass("card-body p-3 forecastBody")
+        const cityDate = $("<h4>").addClass("card-title").text(date.toLocaleDateString('en-US'));
+        const temperature = $("<p>").addClass("card-text forecastTemp").text("Temperature: " + tempF + " °F");
+        const humidity = $("<p>").addClass("card-text forecastHumidity").text("Humidity: " + results[i].main.humidity + "%");
+
+        const image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + results[i].weather[0].icon + ".png")
+
+        cardBody.append(cityDate, image, temperature, humidity);
+        card.append(cardBody);
+        $("#forecast").append(card);
+
+      }
     }
+  });
+
 }
 
-// function getResults (query) {
-//     fetch("${api.base}weather?q=${query}&units=fehrenheit&APPID=${api.key}")
-//     .then(weather => {
-//         return weather.json();
-//     }).then(showResults);
+
+// // const base = "https//api.openweathermap.org/data/2.5/forecast";
+// const city = ("")
+// const apiKey = "&appid=86ea8aa56587bcd972b7b5efee08d7db";
+// const queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
+
+// // let city = $("#searchTerm").val();
+// let date = new Date();
+
+// const searchbox = document.querySelector(".search-box");
+// searchbox.addEventListener("keypress", startQuery);
+
+// function startQuery(event) {  
+//     if (event.keyCode == 13) {
+//         // getResults(searchbox.value);
+//         console.log(searchbox.value)
+//     }
 // }
-
-// function showResults (weather) {
-//     console.log(weather);
-// }
-
-let currentWeather = {
-    fetchWeather: function () {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-        )
-    .then(data) => respond.json())
-    .then(data) => console.log(data);
-    
-},
-
-displayWeather: function (data) {
-    const { name } = data;
-    const { icon, description  } = data.weather;
-    const { temp, humidity } = data.main;
-    const { speed } = data.wind; 
-    console.logc(name, icon, temp, humidity, speed);
-    document.querySelector(".city").innerText = "Weather in " + name;
-    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector(".description").innerText = description;
-    document.querySelector(".temp").innerText = temp + "°F";
-    document.querySelector(".humidity").innerText = "Humidity: " + humidity %;
-    document.querySelector(".wind").innerText = "Wind Speed: " + speed + " ml/h ";
-}                                             
