8bc283940572ec7109752fee1945d28382788b12
stephaniecjohansson <7steph7@gmail.com>
2021-09-18 22:18:28 -0600

    js

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
-const apikey = "86ea8aa56587bcd972b7b5efee08d7db";
-const baseurl = "https://openweathermap.org/api/one-call-api";
+const apiKey = "86ea8aa56587bcd972b7b5efee08d7db";
+const base = "https//api.openweathermap.org/data/2.5/forecast";
 
 
 const searchbox = document.querySelector(".search-box");
-searchbox.addEventListener("keypress", setQuery);
+searchbox.addEventListener("keypress", startQuery);
 
-function setQuery(event) {  
+function startQuery(event) {  
     if (event.keyCode == 13) {
-        // getResults(searchbox.value);
+        getResults(searchbox.value);
         console.log(searchbox.value)
     }
 }
+
+// function getResults (query) {
+//     fetch("${api.base}weather?q=${query}&units=fehrenheit&APPID=${api.key}")
+//     .then(weather => {
+//         return weather.json();
+//     }).then(showResults);
+// }
+
+// function showResults (weather) {
+//     console.log(weather);
+// }
+
+let currentWeather = {
+    fetchWeather: function () {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+        )
+    .then(data) => respond.json())
+    .then(data) => console.log(data);
+    
+},
+
+displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description  } = data.weather;
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind; 
+    console.logc(name, icon, temp, humidity, speed);
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°F";
+    document.querySelector(".humidity").innerText = "Humidity: " + humidity %;
+    document.querySelector(".wind").innerText = "Wind Speed: " + speed + " ml/h ";
+}                                             
